24c73b02b54552069818c3caadef3de68f3f5352
Zalkin <Zalkin.viktor@mail.ru>
2019-04-16 15:16:02 +0300

    Add JSONP request

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 "use strict"
 
-alert("Hello, World");
+function citateResponse(response)
+{
+    alert(response.quoteText);
+}
+
+var requestBody = "method=" + encodeURIComponent("getQuote") + 
+    "&format=" + encodeURIComponent("jsonp") +
+    "&lang=" + encodeURIComponent("ru") + 
+    "&jsonp=" + encodeURIComponent("citateResponse");
+
+var jsonpRequest = document.createElement("script");
+jsonpRequest.src = "https://api.forismatic.com/api/1.0/?" + requestBody;
+jsonpRequest.async = true;
+document.head.appendChild(jsonpRequest);
+
