09a03da0dd5a88d1d140130aabe14b6cdf7f6e87
Zalkin <Zalkin.viktor@mail.ru>
2019-04-16 20:49:36 +0300

    drawImages(). Change image generating from sync to async

--- script.js
+++ script.js
@@ -35,27 +35,6 @@ function createHTML()
 
 function drawImages(canvas)
 {
-    function loadImage(imagesLoaded)
-    {
-        if (imagesLoaded == 4)
-        {
-            return;
-        }
-
-        var x = imagesCrossing.x * (imagesLoaded % 2);
-        var y = imagesCrossing.y * Math.floor(imagesLoaded / 2);
-        var width = imagesLoaded % 2 == 0 ? imagesCrossing.x : canvas.width - imagesCrossing.x;
-        var height = Math.floor(imagesLoaded / 2) == 0 ? imagesCrossing.y : canvas.height - imagesCrossing.y;
-            
-        var img = new Image();
-        img.onload = function() 
-        {   
-            ctx.drawImage(img, x, y, width, height);
-            loadImage(++imagesLoaded);
-        }
-        img.src = "https://source.unsplash.com/collection/190727/" + width + "x" + height;
-    }
-
     var ctx = canvas.getContext("2d");
 
     var imagesCrossing = 
@@ -64,8 +43,22 @@ function drawImages(canvas)
         y: Math.round(Math.random() * canvas.height),
     };
 
-    loadImage(0);
-    
+    for (var i = 0; i < 4; i++)
+    {
+        var img = new Image();
+        img.data = 
+        {
+            x: imagesCrossing.x * (i % 2),
+            y: imagesCrossing.y * Math.floor(i / 2),
+            width: i % 2 == 0 ? imagesCrossing.x : canvas.width - imagesCrossing.x,
+            height: Math.floor(i / 2) == 0 ? imagesCrossing.y : canvas.height - imagesCrossing.y,
+        }
+        img.onload = function() 
+        {
+            ctx.drawImage(this, this.data.x, this.data.y, this.data.width, this.data.height);
+        }
+        img.src = "https://source.unsplash.com/collection/190727/" + img.data.width + "x" + img.data.height;
+    }
 }
 
 function initializeCitate(response)
